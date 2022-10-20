b330e19bdd48a34c3841b01424a152e626c66d32
Uzi <gssdsudas@gmail.com>
2022-10-20 16:43:34 +0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .gitignore
    	modified:   README.md
    	new file:   babel.config.js
    	new file:   jsconfig.json
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/css/fonts/Technology-Bold.ttf
    	new file:   public/css/fonts/Technology-BoldItalic.ttf
    	new file:   public/css/fonts/Technology-Italic.ttf
    	new file:   public/css/fonts/Technology.ttf
    	new file:   public/css/style.css
    	new file:   public/favicon.ico
    	new file:   public/index.html
    	new file:   src/App.vue
    	new file:   src/assets/logo.png
    	new file:   src/main.js
    	new file:   src/scss/_fonts.scss
    	new file:   src/scss/style.scss
    	new file:   vue.config.js
    
    Changes not staged for commit:
    	deleted:    public/css/fonts/Technology-Bold.ttf
    	deleted:    public/css/fonts/Technology-BoldItalic.ttf
    	deleted:    public/css/fonts/Technology-Italic.ttf
    	deleted:    public/css/fonts/Technology.ttf
    	deleted:    public/css/style.css
    	modified:   public/index.html
    	modified:   src/App.vue
    	deleted:    src/assets/logo.png
    	deleted:    src/scss/_fonts.scss
    	deleted:    src/scss/style.scss
    
    Untracked files:
    	src/assets/css.css
    	src/assets/fonts/
    	src/assets/images/

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{time[0]}}:{{time[1]}}:{{time[2]}}
+  </div>
+</template>
+<script>
+import moment from 'moment'
+export default {
+  data(){
+    return{
+      time: []
+    }
+  },
+  mounted(){
+    setInterval(()=>{
+      this.time = [moment().format('HH'), moment().format('mm'), moment().format('ss')]
+    }, 1000)
+  }
+}
+</script>
